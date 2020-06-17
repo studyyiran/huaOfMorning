79aca4d36b9f3057ffa41844353162c33fe765be
yiranzi <570932296@qq.com>
2020-06-17 10:46:41 +0800

    this指向的问题

--- js/promise/v2.js
+++ js/promise/v2.js
@@ -2,6 +2,14 @@
 9:15
  */
 
+/*
+当你需要知道，谁调用的。也就是你希望this是动态的。那你需要通过function
+
+当你不想知道。你确定，this是谁（不需要this也算），哪怕是在调用之前。那你应该用()=>
+
+当你无法使用箭头的时候。（例如，你确实需要适当的动态指定。例如原型方法。）那你就可以替原型方法做主，bind上去
+ */
+
 
 function MyPromise(func) {
     this.status = 'padding'
@@ -13,13 +21,12 @@ function MyPromise(func) {
 
     function _then(whenResolveRunTheFunc) {
         //
-        const thenBackPromise = new MyPromise(function (resolve, reject) {
+        const thenBackPromise = new MyPromise( (resolve, reject) => {
             this.thenArr.push(function (pValue) {
                 // 为了then内在的东西要执行
                 const thenReturnValue = whenResolveRunTheFunc(pValue)
-                console.log(thenReturnValue)
                 // 为了then后面的
-                // resolve(thenReturnValue)
+                resolve(thenReturnValue)
             })
         })
         return thenBackPromise
@@ -31,37 +38,40 @@ function MyPromise(func) {
 
     // 这是唯一可以改变状态的
     function _outCallMeResolve(resolveValue) {
-
-        function init() {
+        const init = () => {
             // 首先变更状态
             this.status = 'resolve';
             // 修改数值
             this.promiseValue = resolveValue
             // 然后执行then的回调
-            setTimeout(function () {
+            setTimeout(() => {
                 this.thenArr.forEach(function (func) {
+                    func(resolveValue)
                 })
             })
         }
 
         // (如果是promise呢？)那我应该尊重他
         if (resolveValue instanceof MyPromise) {
-            console.log('get it1')
             // 等你完成后，我再变更状态
-            resolveValue.then(function () {
+            resolveValue.then( () => {
                 init()
             })
         } else {
-            console.log('get it2')
             init()
         }
     }
 
     if (!MyPromise.prototype.then) {
         MyPromise.prototype.then = _then
+        MyPromise.prototype._outCallMeResolve = _outCallMeResolve
+        MyPromise.prototype._outCallMeReject = _outCallMeReject
     }
     try {
-        func(_outCallMeResolve,  _outCallMeReject)
+        // 这种调用无效居然。哪怕你指了this也没用。
+        // func(this._outCallMeResolve,  this._outCallMeReject)
+        // bind对于箭头函数是无能为力的
+        func(this._outCallMeResolve.bind(this),  this._outCallMeReject.bind(this))
     } catch (e) {
         console.error(e)
     }
@@ -93,26 +103,29 @@ const a = (string) => {
 
 const b = () => {
     const p1 = new MyPromise((resolve) => {
-        console.log('2')
+        console.log('run')
         resolve('p1 is best')
     });
     const p2 = p1.then((value) => {
-        console.log('get it' + value)
+        console.log('get it 1 ' + value)
         // return new Promise(() => {
         //
+
+
+
         // })
         return 'is p1.then'
         // return Promise.resolve();
     })
     const p3 = p2.then((value) => {
-        console.log('get it' + value)
+        console.log('get it 2 ' + value)
         return 'is p2.then'
         // return Promise.resolve();
     })
 }
 
 // a('Test then is still promise ? ')
-b('Test then is still promise ? ')
+b('then can pass ')
 /*
 1
  */
